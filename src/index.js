1445ed3a45dd0f2a30f24b6cfb8e75dfccdc006f
DolphiN1000 <88268032+DolphiN1000@users.noreply.github.com>
2022-09-10 17:05:39 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import './js/swiper';
+import './js/slider';
+import './js/modal';
+import './js/mobile-menu.js';
+import './js/data-menu';
+import './js/anchor-logic-mobile';
+import './js/modal-location';
+import './js/modal-franchise';
+import './js/read-more';
+import './js/modal-products1';
+import './js/modal-products2';
+import './js/modal-products3';
+
+
